46eab856260158a6a84b7a0d69eba7dbe9b44189
Munevver BAHAR <45920578+1903mbjk@users.noreply.github.com>
2020-10-28 16:02:20 +0300

    Navigation Part 1

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Main from './components/MainComponent';
 
+//import 'react-native-gesture-handler';
+
 export default class App extends React.Component {
   render() {
     return (
