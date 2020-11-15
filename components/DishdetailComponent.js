95a59c783d1d56e653537091b9d5762f37f50607
Munevver BAHAR <45920578+1903mbjk@users.noreply.github.com>
2020-11-15 19:53:48 +0300

    Social Sharing

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -9,6 +9,7 @@ import {
   Button,
   Alert,
   PanResponder,
+  Share,
 } from "react-native";
 import { Card, Icon, Rating, Input } from "react-native-elements";
 import { connect } from "react-redux";
@@ -76,6 +77,19 @@ function RenderDish({ dish, favorite, markFavorite, openCommentForm }) {
       }
     },
   });
+
+  const shareDish = (title, message, url) => {
+    Share.share(
+      {
+        title: title,
+        message: title + ": " + message + " " + url,
+        url: url,
+      },
+      {
+        dialogTitle: "Share " + title,
+      }
+    );
+  };
   if (dish != null) {
     return (
       <Animatable.View
@@ -105,6 +119,16 @@ function RenderDish({ dish, favorite, markFavorite, openCommentForm }) {
               type="font-awesome"
               color="#512DA8"
               onPress={() => openCommentForm()}
+            />
+            <Icon
+              raised
+              reverse
+              name="share"
+              type="font-awesome"
+              color="#51D2A8"
+              onPress={() =>
+                shareDish(dish.name, dish.description, baseUrl + dish.image)
+              }
             />
           </View>
         </Card>
