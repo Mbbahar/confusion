76993520635defded40d167001e3c8a43653c471
Munevver BAHAR <45920578+1903mbjk@users.noreply.github.com>
2020-11-15 19:44:52 +0300

    Sending Email

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,24 +1,26 @@
 import React, { Component } from "react";
 import { ScrollView, View, Text } from "react-native";
-import { Card } from "react-native-elements";
+import { Card, Button, Icon } from "react-native-elements";
 import { CONTACTS } from "../shared/contacts";
-import * as Animatable from 'react-native-animatable';
+import * as Animatable from "react-native-animatable";
+import * as MailComposer from "expo-mail-composer";
 
 function RenderItem(props) {
   const item = props.item;
 
   if (item != null) {
     return (
-      <Animatable.View animation="fadeInDown" duration={2000} delay={1000} >  
-      <Card title="Contact Information">
-        <Text style={{ margin: 5 }}>{item.name}</Text>
-        <Text style={{ margin: 5 }}>{item.image}</Text>
-        <Text style={{ margin: 5 }}>{item.label}</Text>
-        <Text style={{ margin: 5 }}>Tel: {item.price}</Text>
-        <Text style={{ margin: 5 }}>Fax: {item.featured}</Text>
-        <Text style={{ margin: 5 }}>Email: {item.description}</Text>
-      </Card>
-      </Animatable.View>
+      
+        <Card title="Contact Information">
+          <Text style={{ margin: 5 }}>{item.name}</Text>
+          <Text style={{ margin: 5 }}>{item.image}</Text>
+          <Text style={{ margin: 5 }}>{item.label}</Text>
+          <Text style={{ margin: 5 }}>Tel: {item.price}</Text>
+          <Text style={{ margin: 5 }}>Fax: {item.featured}</Text>
+          <Text style={{ margin: 5 }}>Email: {item.description}</Text>
+          
+        </Card>
+    
     );
   } else {
     return <View></View>;
@@ -36,12 +38,29 @@ class Contact extends Component {
   static navigationOptions = {
     title: "Contact",
   };
+
+  sendMail() {
+    MailComposer.composeAsync({
+      recipients: ["confusion@food.net"],
+      subject: "Enquiry",
+      body: "To whom it may concern:",
+    });
+  }
+
   render() {
     return (
       <ScrollView>
+        <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
         <RenderItem
           item={this.state.contacts.filter((contact) => contact.featured)[0]}
         />
+        <Button
+          title="Send Email"
+          buttonStyle={{ backgroundColor: "#512DA8" }}
+          icon={<Icon name="envelope-o" type="font-awesome" color="white" />}
+          onPress={this.sendMail}
+        />
+        </Animatable.View>
       </ScrollView>
     );
   }
